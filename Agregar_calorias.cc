b84fac6bb61907906a90c289cc1dd804c9cc97cc
JoseGuardado67 <00198526@uca.edu.sv>
2026-09-09 15:44:01 -0600

    modificar rama consultar

--- Agregar_calorias.cc
+++ Agregar_calorias.cc
@@ -1,40 +1,13 @@
 #include <iostream>
 
 int ConsultarCalorias(float calorias);
-int AgregarCalorias(int *calorias, int *cant);
 
 int main()
 {
     int calorias = 2000;
-    int cant = 0;
-
-    std::cout << "Ingrese la cantidad de calorias consumidas: ";
-    std::cin >> cant;
-
-    if (cant < 0 || cant == 0)
-    {
-        std::cout <<std::endl<< "ERROR INGRESE UNA CANTIDAD MAYOR A 0" << std::endl;
-
-        do
-        {
-            std::cout << "Ingrese la cantidad de calorias consumidas (mayor a 0): ";
-            std::cin >> cant;
-        } while (cant < 0 || cant == 0);
-    }
-
-    AgregarCalorias(&calorias, &cant);
-
-    std::cout << "Calorias agregadas con exito." << std::endl;
-
-    ConsultarCalorias(calorias);
+ ConsultarCalorias(calorias);
 }
 
-int AgregarCalorias(int *calorias, int *cant)
-{
-    *calorias += *cant;
-
-    return *calorias;
-}
 
 int ConsultarCalorias(float calorias)
 {
