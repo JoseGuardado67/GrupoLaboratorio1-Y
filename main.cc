7f4024127aecd0c3f89674ee22351567a65928a4
Yohalmo Daniel Cruz Peréz <00033626@uca.edu.sv>
2026-09-09 14:51:40 -0600

    Agregando La funcion Quemar Calorias
    funcion totalmente bien.

--- main.cc
+++ main.cc
@@ -1,6 +1,23 @@
     #include <iostream>
+
+int QuemarCalorias(int* calorias, int* caloriasQuemadas);
+
+int main() {
+    int calorias = 2000;
+    int cant = 0;
+
+    std::cout << "Ingrese las calorias a quemar: ";
+    std::cin >> cant;
+
+    QuemarCalorias(&calorias, &cant);
+    std::cout << "Calorias quemadas: " << cant << std::endl;
+    std::cout << "Calorias restantes: " << calorias << std::endl;
+
+    return 0;
+}
+
+int QuemarCalorias(int* calorias, int* cant) {
+    *calorias -= *cant;
+    return *calorias;
+}
     
-    int main() {
-      
-      return 0;
-    }
