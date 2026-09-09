4717c7297c615914b6f4cedc686d5514a912170b
JoseGuardado67 <00198526@uca.edu.sv>
2026-09-09 15:20:10 -0600

    agregar funcion consultar calorias

--- Agregar_calorias.cc
+++ Agregar_calorias.cc
@@ -1,26 +1,43 @@
 #include <iostream>
 
-
-
+int ConsultarCalorias(float calorias);
 int AgregarCalorias(int *calorias, int *cant);
 
 int main()
 {
-     int calorias = 2000;
-     int cant= 0;
+    int calorias = 2000;
+    int cant = 0;
 
-    std::cout << "Ingrese la cantidad de calorias consumidas: " << std::endl;
+    std::cout << "Ingrese la cantidad de calorias consumidas: ";
     std::cin >> cant;
 
-   AgregarCalorias(&calorias, &cant);
+    if (cant < 0 || cant == 0)
+    {
+        std::cout <<std::endl<< "ERROR INGRESE UNA CANTIDAD MAYOR A 0" << std::endl;
 
-   std:: cout<<"Calorias agregadas: "<< cant << std::endl;
-   std:: cout<<"Calorias total: "<<calorias<<std::endl;
+        do
+        {
+            std::cout << "Ingrese la cantidad de calorias consumidas (mayor a 0): ";
+            std::cin >> cant;
+        } while (cant < 0 || cant == 0);
+    }
+
+    AgregarCalorias(&calorias, &cant);
+
+    std::cout << "Calorias agregadas con exito." << std::endl;
+
+    ConsultarCalorias(calorias);
 }
 
 int AgregarCalorias(int *calorias, int *cant)
 {
- *calorias+=*cant;
+    *calorias += *cant;
 
     return *calorias;
+}
+
+int ConsultarCalorias(float calorias)
+{
+    std::cout << "Calorias totales: " << calorias;
+    return calorias;
 }
