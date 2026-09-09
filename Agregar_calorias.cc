0b47e726a988b0c28bce43bd5facc7579b76855d
JoseGuardado67 <00198526@uca.edu.sv>
2026-09-09 14:50:07 -0600

    actualizar funcion

--- Agregar_calorias.cc
+++ Agregar_calorias.cc
@@ -1,2 +1,26 @@
-#include<iostream>
+#include <iostream>
 
+
+
+int AgregarCalorias(int *calorias, int *cant);
+
+int main()
+{
+     int calorias = 2000;
+     int cant= 0;
+
+    std::cout << "Ingrese la cantidad de calorias consumidas: " << std::endl;
+    std::cin >> cant;
+
+   AgregarCalorias(&calorias, &cant);
+
+   std:: cout<<"Calorias agregadas: "<< cant << std::endl;
+   std:: cout<<"Calorias total: "<<calorias<<std::endl;
+}
+
+int AgregarCalorias(int *calorias, int *cant)
+{
+ *calorias+=*cant;
+
+    return *calorias;
+}
